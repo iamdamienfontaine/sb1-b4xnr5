2e4737bf4730f5e56d59e9511dfec4cc12a54950
iamdamienfontaine <git@stackblitz.com>
2024-10-10 12:54:43 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# sb1-b4xnr5
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/iamdamienfontaine/sb1-b4xnr5)
---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { ThemeProvider } from "@/components/theme-provider"
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'TiBook - Réservation Multi-domaine',
+  description: 'TiBook est une application de réservation multi-domaine pour restaurants, coworking, hôtels, et plus encore.',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="fr">
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
